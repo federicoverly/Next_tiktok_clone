46ee49db53e51e7b4c2106dcb209adcd354a5751
Federico Verly <federicoverly@gmail.com>
2022-09-13 15:42:56 +0200

    videoCart

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["scontent-vie1-1.xx.fbcdn.net", "images.unsplash.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
+import axios from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import NoResults from "../components/NoResults";
+import VideoCart from "../components/VideoCart";
+import { Video } from "../types";
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {" "}
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCart post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No videos"} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
