832d3b144830344a5b403a0d202187dcc63f2fb5
Federico Verly <federicoverly@gmail.com>
2022-09-14 15:49:03 +0200

    upload_video functionality

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["scontent-vie1-1.xx.fbcdn.net", "images.unsplash.com"],
+    domains: [
+      "scontent-vie1-1.xx.fbcdn.net",
+      "images.unsplash.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
